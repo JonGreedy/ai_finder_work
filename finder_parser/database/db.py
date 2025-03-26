d71b575a109d5d9e48092b7b9f7b5dc8efe04bd1
Jon Greedy <jon.greedy@gmail.com>
2025-03-26 18:12:03 +0300

    Edit all

--- finder_parser/database/db.py
+++ finder_parser/database/db.py
@@ -3,18 +3,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./finder_parser/database/vacancies.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SQLALCHEMY_DATABASE_URL = "sqlite:///../vacancies.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
 
 # Создание таблиц
 Base.metadata.create_all(bind=engine)
 
+
 def get_db():
-    db = SessionLocal()
+    db = Session()
     try:
         yield db
     finally:
